dcbd5a766f1cc7a8622401b136c88a9a31a42548
RajiBabu <rajalakshmi.babu@sjsu.edu>
2020-04-07 17:09:06 -0700

    Trigger

---
+++ application/backend/src/main/java/masterproject/backend/model/TriggerML.java
@@ -0,0 +1,20 @@
+package masterproject.backend.model;
+
+public class TriggerML {
+	
+	String userId;
+	String period;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+}
